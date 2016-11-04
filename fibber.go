9ebfad3f2e9948bb87a52552914449145abf8812
Lloyd <lloydmeta@gmail.com>
2016-11-04 20:39:58 +0900

    Rename temp to sum
    Slight refactoring

--- fibber.go
+++ fibber.go
@@ -6,20 +6,17 @@ import (
 
 // Of returns the Fibonacci number at a given index
 func Of(to uint64) *big.Int {
-	if to == 0 {
-		return big.NewInt(0)
-	} else if to == 1 {
-		return big.NewInt(1)
-	} else {
-		prev := big.NewInt(0)
-		current := big.NewInt(1)
-		var i uint64 = 1
-		for ; i < to; i++ {
-			temp := big.NewInt(0)
-			temp.Add(prev, current)
-			prev = current
-			current = temp
-		}
-		return current
+	if to <= 1 {
+		return big.NewInt(int64(to))
 	}
+	prev := big.NewInt(0)
+	current := big.NewInt(1)
+	var i uint64 = 2
+	for ; i <= to; i++ {
+		sum := big.NewInt(0)
+		sum.Add(prev, current)
+		prev = current
+		current = sum
+	}
+	return current
 }
